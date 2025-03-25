af4332420b0ccb4dd34559a00490d346d2bfbf23
Aaron Escalona <escalona.ap@gmail.com>
2025-03-25 15:02:15 -0700

    update

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,7 +2,7 @@ import type {EntryContext, AppLoadContext} from '@shopify/remix-oxygen';
 import {RemixServer} from '@remix-run/react';
 import {isbot} from 'isbot';
 import {renderToReadableStream} from 'react-dom/server';
-import {createContentSecurityPolicy} from '@shopify/hydrogen';
+// import {createContentSecurityPolicy} from '@shopify/hydrogen';
 
 export default async function handleRequest(
   request: Request,
@@ -11,19 +11,16 @@ export default async function handleRequest(
   remixContext: EntryContext,
   context: AppLoadContext,
 ) {
-  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
-    shop: {
-      checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
-      storeDomain: context.env.PUBLIC_STORE_DOMAIN,
-    },
-  });
+  // const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+  //   shop: {
+  //     checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
+  //     storeDomain: context.env.PUBLIC_STORE_DOMAIN,
+  //   },
+  // });
 
   const body = await renderToReadableStream(
-    <NonceProvider>
-      <RemixServer context={remixContext} url={request.url} nonce={nonce} />
-    </NonceProvider>,
+    <RemixServer context={remixContext} url={request.url} />,
     {
-      nonce,
       signal: request.signal,
       onError(error) {
         console.error(error);
